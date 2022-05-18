56f305a38d6cf9f6936d9fe80569c0990195dcf1
mateus-kent <mateusquent2003@gmail.com>
2022-05-18 11:48:14 -0300

    feat: add cors

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import express from 'express'
 import routes from './routes'
+import cors from 'cors'
 
 const app = express()
 
+app.use(cors())
+
 app.use(express.json())
 app.use(routes)
-
-console.log(process.env.MAIL_SERVER)
 
 app.listen(3000, () => console.log('Server is running in the port 3000 🚀 🚀'))
